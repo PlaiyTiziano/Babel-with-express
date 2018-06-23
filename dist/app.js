aa614730c28c6eb41c80ea90b4420a49782d0460
PlaiyTiziano <tiziano.plaiy@student.howest.be>
2018-06-23 14:19:37 +0200

    removed dist folder

--- dist/app.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var _test = require('./test');
-
-require('./Express/express');
-
-(0, _test.test)("Hello world");
-//# sourceMappingURL=app.js.map
--- dist/test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var test = function test(value) {
-  return console.log(value);
-};
-
-module.exports = { test: test };
-//# sourceMappingURL=test.js.map
