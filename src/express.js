e51dfd09a993494c2bd7bd574928f26eca16e960
PlaiyTiziano <tiziano.plaiy@student.howest.be>
2018-08-13 19:55:03 +0200

    Updated .babelrc so arrow functions are possible in classes.

--- src/express.js
+++
@@ -1,5 +0,0 @@
-import express from 'express';
-
-const app = express();
-
-app.listen(1337, () => console.log('Server is running'));
---
+++ src/server/server.js
@@ -0,0 +1,5 @@
+import express from 'express';
+
+const app = express();
+
+app.listen(1337, () => console.log('Server is running on port 1337'));
